bfdcaecce7ad45a1f94ed946104093eff3e95b20
del-operator <114185271+del-operator@users.noreply.github.com>
2022-10-22 13:07:23 -0500

    Changed solidity version to 0.8.16 from 0.8.15

--- test/interfaces/IMessagePassing.t.sol
+++ test/interfaces/IMessagePassing.t.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.16;
 
 interface IMessagePassing {
     function setTrustedRemote(uint16 _srcChainId, bytes calldata _srcAddress) external;
