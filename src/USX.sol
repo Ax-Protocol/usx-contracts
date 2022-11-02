766f8300c72371f5624e4d4864ff858936888f82
del-operator <114185271+del-operator@users.noreply.github.com>
2022-11-02 14:22:47 -0500

    CI test.

--- src/USX.sol
+++ src/USX.sol
@@ -29,7 +29,7 @@ contract USX is Initializable, UUPSUpgradeable, Ownable, OERC20, IUSX {
     }
 
     /**
-     * @dev This empty reserved space is put in place to allow future versions to add new
+     * @dev This empty, reserved space is put in place to allow future versions to add new
      * variables without shifting down storage in the inheritance chain.
      */
     uint256[50] private __gap;
