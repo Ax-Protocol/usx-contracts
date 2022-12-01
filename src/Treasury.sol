b65deaec4ef7beddfe2fafdb126d7b1d1c3f3560
0xhashmap <114262299+0xhashmap@users.noreply.github>
2022-12-01 00:55:48 -0600

    added 3CRV staking and unstaking

--- src/Treasury.sol
+++ src/Treasury.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.16;
 
 import "solmate/utils/SafeTransferLib.sol";
 import "./interfaces/IStableSwap3Pool.sol";
+import "./interfaces/ILiquidityGauge.sol";
 import "./proxy/UUPSUpgradeable.sol";
 import "./interfaces/IERC20.sol";
 import "./utils/Ownable.sol";
@@ -18,7 +19,8 @@ contract Treasury is Ownable, UUPSUpgradeable, ITreasury {
     // Storage Variables
     address public usxToken;
     address public stableSwap3PoolAddress;
-    address public curveToken;
+    address public liquidityGaugeAddress;
+    address public backingToken;
     mapping(address => SupportedStable) public supportedStables;
     uint256 public previousLpTokenPrice;
 
@@ -26,11 +28,12 @@ contract Treasury is Ownable, UUPSUpgradeable, ITreasury {
     event Mint(address indexed account, uint256 amount);
     event Redemption(address indexed account, uint256 amount);
 
-    function initialize(address _stableSwap3PoolAddress, address _usxToken, address _curveToken) public initializer {
+    function initialize(address _stableSwap3PoolAddress, address _liquidityGaugeAddress, address _usxToken, address _backingToken) public initializer {
         __Ownable_init();
         /// @dev No constructor, so initialize Ownable explicitly.
         stableSwap3PoolAddress = _stableSwap3PoolAddress;
-        curveToken = _curveToken;
+        liquidityGaugeAddress= _liquidityGaugeAddress;
+        backingToken = _backingToken;
         usxToken = _usxToken;
     }
 
@@ -45,15 +48,15 @@ contract Treasury is Ownable, UUPSUpgradeable, ITreasury {
      * @param _amount The amount of the input token used to mint USX.
      */
     function mint(address _stable, uint256 _amount) public {
-        require(supportedStables[_stable].supported || _stable == curveToken, "Unsupported stable.");
+        require(supportedStables[_stable].supported || _stable == backingToken, "Unsupported stable.");
 
         // Obtain user's input tokens
         SafeTransferLib.safeTransferFrom(ERC20(_stable), msg.sender, address(this), _amount);
 
-        uint256 lpTokens;
-        if (_stable != curveToken) {
+        uint256 lpTokenAmount;
+        if (_stable != backingToken) {
             // Obtain contract's LP token balance before adding liquidity
-            uint256 preBalance = IERC20(curveToken).balanceOf(address(this));
+            uint256 preBalance = IERC20(backingToken).balanceOf(address(this));
 
             // Add liquidity to Curve
             SafeTransferLib.safeApprove(ERC20(_stable), stableSwap3PoolAddress, _amount);
@@ -62,23 +65,16 @@ contract Treasury is Ownable, UUPSUpgradeable, ITreasury {
             IStableSwap3Pool(stableSwap3PoolAddress).add_liquidity(amounts, 0);
 
             // Calculate the amount of LP tokens received from adding liquidity
-            lpTokens = IERC20(curveToken).balanceOf(address(this)) - preBalance;
+            lpTokenAmount = IERC20(backingToken).balanceOf(address(this)) - preBalance;
         } else {
-            lpTokens = _amount;
+            lpTokenAmount = _amount;
         }
 
-        // Obtain current LP token virtual price (3CRV:USX conversion factor)
-        uint256 lpTokenPrice = IStableSwap3Pool(stableSwap3PoolAddress).get_virtual_price();
+        // Stake LP tokens
+        stakeLpTokens(lpTokenAmount);
 
-        // Don't allow LP token price to decrease
-        if (lpTokenPrice < previousLpTokenPrice) {
-            lpTokenPrice = previousLpTokenPrice;
-        } else {
-            previousLpTokenPrice = lpTokenPrice;
-        }
-
-        // Mint USX tokens
-        uint256 mintAmount = (lpTokens * lpTokenPrice) / 1e18;
+        // Mint USX
+        uint256 mintAmount = getMintAmount(lpTokenAmount);
         IUSX(usxToken).mint(msg.sender, mintAmount);
         emit Mint(msg.sender, mintAmount);
     }
@@ -90,9 +86,65 @@ contract Treasury is Ownable, UUPSUpgradeable, ITreasury {
      * @param _amount The amount of USX tokens to burn upon redemption.
      */
     function redeem(address _stable, uint256 _amount) public {
-        require(supportedStables[_stable].supported || _stable == curveToken, "Unsupported stable.");
+        require(supportedStables[_stable].supported || _stable == backingToken, "Unsupported stable.");
 
-        // Obtain current LP token virtual price (3CRV:USX conversion factor)
+        // Get amount of LP token based on USX burn amount
+        uint256 lpTokenAmount = getLpTokenAmount(_amount);
+
+        // Unstake LP tokens
+        unstakeLpTokens(lpTokenAmount);
+
+        uint256 redeemAmount;
+        if (_stable != backingToken) {
+            // Obtain contract's withdrawal token balance before removing liquidity
+            uint256 preBalance = IERC20(_stable).balanceOf(address(this));
+
+            // Remove liquidity from Curve
+            IStableSwap3Pool(stableSwap3PoolAddress).remove_liquidity_one_coin(
+                lpTokenAmount, supportedStables[_stable].curveIndex, 0
+            );
+
+            // Calculate the amount of stablecoin received from removing liquidity
+            redeemAmount = IERC20(_stable).balanceOf(address(this)) - preBalance;
+        } else {
+            redeemAmount = lpTokenAmount;
+        }
+
+        // Transfer desired withdrawal tokens to user
+        SafeTransferLib.safeTransfer(ERC20(_stable), msg.sender, redeemAmount);
+
+        // Burn USX tokens
+        IUSX(usxToken).burn(msg.sender, _amount);
+        emit Redemption(msg.sender, _amount);
+    }
+
+    function stakeLpTokens(uint256 _amount) private {
+        // Approve LiquidityGauge to spend Treasury's 3CRV
+        IERC20(backingToken).approve(liquidityGaugeAddress, _amount);
+
+        // Deposit 3CRV into LiquidityGauge
+        ILiquidityGauge(liquidityGaugeAddress).deposit(_amount);
+    }
+
+    function unstakeLpTokens(uint256 _amount) private {
+        // Withdraw 3CRV from LiquidityGauge
+        ILiquidityGauge(liquidityGaugeAddress).withdraw(_amount);
+    }
+
+    function getMintAmount(uint256 _lpTokenAmount) private returns (uint256 mintAmount) {
+        uint256 lpTokenPrice = IStableSwap3Pool(stableSwap3PoolAddress).get_virtual_price();
+
+        // Don't allow LP token price to decrease
+        if (lpTokenPrice < previousLpTokenPrice) {
+            lpTokenPrice = previousLpTokenPrice;
+        } else {
+            previousLpTokenPrice = lpTokenPrice;
+        }
+
+        mintAmount = (_lpTokenAmount * lpTokenPrice) / 1e18;
+    }
+
+    function getLpTokenAmount(uint256 _amount) private returns (uint256 lpTokenAmount) {
         uint256 lpTokenPrice = IStableSwap3Pool(stableSwap3PoolAddress).get_virtual_price();
 
         // Don't allow LP token price to decrease
@@ -103,31 +155,8 @@ contract Treasury is Ownable, UUPSUpgradeable, ITreasury {
         }
 
         uint256 conversionFactor = (1e18 * 1e18 / lpTokenPrice);
-        uint256 lpTokens = (_amount * conversionFactor) / 1e18;
-
-        uint256 redeemAmount;
-        if (_stable != curveToken) {
-            // Obtain contract's withdrawal token balance before removing liquidity
-            uint256 preBalance = IERC20(_stable).balanceOf(address(this));
-
-            // Remove liquidity from Curve
-            IStableSwap3Pool(stableSwap3PoolAddress).remove_liquidity_one_coin(
-                lpTokens, supportedStables[_stable].curveIndex, 0
-            );
-
-            // Calculate the amount of stablecoin received from removing liquidity
-            redeemAmount = IERC20(_stable).balanceOf(address(this)) - preBalance;
-        } else {
-            redeemAmount = lpTokens;
-        }
-
-        // Transfer desired withdrawal tokens to user
-        SafeTransferLib.safeTransfer(ERC20(_stable), msg.sender, redeemAmount);
-
-        // Burn USX tokens
-        IUSX(usxToken).burn(msg.sender, _amount);
-        emit Redemption(msg.sender, _amount);
-    }
+        lpTokenAmount = (_amount * conversionFactor) / 1e18;
+    } 
 
     /* ****************************************************************************
     **
---
+++ src/interfaces/ILiquidityGauge.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+interface ILiquidityGauge{
+    function deposit(uint256 _amount) external;
+
+    function withdraw(uint256 _amount) external;
+}
